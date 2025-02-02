68d2fa79abecaca3d54b88f44cad8e817e94d409
4r33x <vonauwers@gmail.com>
2025-02-02 13:35:35 +0300

    first commit

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut cfg = tonic_build::configure();
+    #[cfg(feature = "client")]
+    {
+        cfg = cfg.build_client(true).build_server(false);
+    }
+
+    #[cfg(feature = "server")]
+    {
+        cfg = cfg.build_server(true);
+    }
+
+    cfg.compile_protos(&["cabal.proto"], &["../cabal_proto"])
+}
